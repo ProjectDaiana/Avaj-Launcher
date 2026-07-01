0af3c6f9cc784cf500868287cb2139a13df28846
Daiana <dairotchen@gmail.com>
2026-07-01 12:13:11 +0200

    refactor: rename Baloon class to Balloon and update file structure

---
+++ src/ro/academyplus/avaj/simulator/Balloon.java
@@ -0,0 +1,6 @@
+package ro.academyplus.avaj.simulator;
+
+public class Balloon extends Aircraft  {
+    public Balloon(long p_id, String p_name, Coordinates p_coordinate);
+    public void updateConditions();
+}
--- src/ro/academyplus/avaj/simulator/Baloon.java
+++
@@ -1,6 +0,0 @@
-package ro.academyplus.avaj.simulator;
-
-public class Baloon extends Aircraft  {
-    public Baloon(long p_id, String p_name, Coordinates p_coordinate);
-    public void updateConditions();
-}
