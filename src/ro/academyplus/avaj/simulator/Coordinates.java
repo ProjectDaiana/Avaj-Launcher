f5c231359b39a23e4b5db2664caf9b87dd525bd6
Daiana <dairotchen@gmail.com>
2026-07-03 09:49:22 +0200

    fix(Coordinates): conditional check for negative coordinates, implement getters

--- src/ro/academyplus/avaj/simulator/Coordinates.java
+++ src/ro/academyplus/avaj/simulator/Coordinates.java
@@ -6,9 +6,19 @@ public class Coordinates {
     private int height;
 
     Coordinates(int p_longitude, int p_latitude, int p_height){
-        if (p_longitude || p_latitude || p_height < 0 )
+        if (p_longitude < 0 || p_latitude < 0 || p_height < 0)
+            throw new IllegalArgumentException("Coordinates cannot be negative.");
+        this.longitude = p_longitude;
+        this.latitude = p_latitude;
+        this.height = Math.min(p_height, 100);
     }
-    public int getLongitude();
-    public int getLatitude();
-    public int getHeight();
+    public int getLongitude() {
+        return longitude;
+    }
+    public int getLatitude() {
+        return latitude;
+    }
+    public int getHeight() {
+        return height;
+    }
 }
